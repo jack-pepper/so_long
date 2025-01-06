60474b45dd8e43e9645fd1683eb1695d9d39807f
jack-pepper <70756191+jack-pepper@users.noreply.github.com>
2025-01-06 22:32:03 +0100

    add libft

---
+++ lib/libft/libsort/ft_qck_srt.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_qck_srt.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmalie <mmalie@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 22:51:56 by mmalie            #+#    #+#             */
+/*   Updated: 2025/01/04 00:28:42 by mmalie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+/*
+ * QUICK SORT is a divide-and-conquer algorithm that selects a pivot and
+ * partitions the array into smaller sub-arrays.
+ *
+ * STEPS:
+ * - Select a pivot (first, last, random, median...). 
+ * - Partition the array around the pivot: put all elements smaller than the
+ *      pivot on its left, all greater on its right.The index of the pivot
+ *      is thus obtained.
+ * - Recursively call the same process to the two subarrays.
+ * - Base case: stop when there is only one element left in subarray.
+ * -
+ */
+/*
+void	ft_qck_srt(int *arr, size_t size)
+{
+}
+*/
